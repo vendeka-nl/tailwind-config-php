b50b064fc157b2aea57fd1a139b7b171ef552489
Roy van Veldhuizen <18717340+royvanv@users.noreply.github.com>
2020-05-18 14:55:26 +0200

    Actually map the object (solves issue #1).

--- index.js
+++ index.js
@@ -16,9 +16,29 @@ const tailwindConfig = require(path.normalize(`${process.cwd()}/${argv.config}`)
 const fs = require('fs');
 
 const fullConfig = resolveConfig(tailwindConfig);
-const configJson = JSON.stringify(fullConfig);
 
-fs.writeFileSync(argv.output, `<?php
-return json_decode('${configJson}');`);
+function objectToPhpObjectArray (obj, depth = 0) {
+    const indent = '    ';
+    let arr = `(object) [\n`;
+
+    for (const key in obj) {
+        let value = obj[key] == null || typeof obj[key] === 'function' ? null : obj[key];
+        let str;
+
+        if (value && typeof value === 'object') {
+            str = objectToPhpObjectArray(value, depth + 1);
+        } else {
+            str = JSON.stringify(value);
+        }
+        
+        arr += `${indent.repeat(depth + 1)}'${key}' => ${str},\n`;
+    }
+
+    arr += `${indent.repeat(depth)}]`;
+
+    return arr;
+}
+
+fs.writeFileSync(argv.output, `<?php\nreturn ${objectToPhpObjectArray(fullConfig)};`);
 
 console.log('Tailwind config written to ' + argv.output);
